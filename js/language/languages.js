1e670df99cb0d79cc79f7c5bfb2dd6164c1740a0
RaheelTheDeveloper <raheel.ali.developer.com>
2021-03-07 19:18:22 +0100

    UI Project

---
+++ js/language/languages.js
@@ -0,0 +1,211 @@
+const DB_LANGUAGES = {
+  en: {
+    title: "The Flying Dutchman",
+    welcomeMessage: "Welcome to the Flying Dutchman!",
+    "login-h1": "Login",
+    undoButton: "Undo",
+    redoButton: "Redo",
+    resetButton: "Reset Database",
+    HistoryBtn: "History",
+    InventoryBtn: "Inventory",
+    username: "User Name",
+    password: "Password",
+    signin: "Sign In",
+    "signup-h1": "Sign Up",
+    allTab: "All",
+    beerTab: "Beers",
+    spiritTab: "Spirits",
+    wineTab: "Wines",
+    customerButton : "Customer",
+    barButton : "Bartender",
+    checkout: "Checkout",
+    orderHeading: "My Orders",
+    credit: "Credit: ",
+    yes: "Yes",
+    no: "No",
+    recharge: "Amount",
+    rechargeButton: "Recharge",
+    signout: "Signout",
+    vip: "VIP Login",
+    availability: "In Stock : ",
+    menuTabs: {
+      allDrink: "All Drinks",
+      beer: "Beer",
+      wine: "Wine",
+      spirit: "Spirits",
+      myFavourite: "My favourites"
+    },
+    cashier: {
+      cashierText: "Checkout",
+      payButton: "Pay",
+      receiptButton: "Receipt",
+      checkoutText: "Drop the order here to pay!"
+    },
+    table: {
+      t1Name: "Table 1",
+      t2Name: "Table 2",
+      t3Name: "Table 3"
+    },
+	Inventory: {
+		OrderID: "Order ID",
+		BeverageName: "Beverage Name",
+		Amount: "Amount",
+		Alcohol: "Alcohol",
+		Category: "Category",
+		Quantity: "Quantity"
+		
+	},
+	OrderHistory: {
+		OrderID: "Order ID",
+		BeverageName: "Beverage Name",
+		OrderBy : "Order By",
+		Amount:"Amount",
+		Alcohol:"Alcohol",
+		OrderTime: "Order Time",
+		Category:"Category"
+	},
+    order: {
+      orderText: "Total Amount: "
+    },
+	Report:{
+		ReportIssue: "Report Issue"
+	},
+  bartenderHome: {
+   'ln-home-menu' : 'Home',
+ 'ln-history-menu' : 'History',
+ 'ln-inventory-menu' : 'Inventory',
+ 'ln-logout-menu' : 'Logout',
+ 'ln-table1-text': 'Table 1',
+ 'ln-total-amount-text1': 'Total Amount',
+ 'ln-total-amount-text2': 'Total Amount',
+ 'ln-total-amount-text3': 'Total Amount',
+ 'ln-table2-text' : 'Table 2',
+ 'ln-table3-text' : 'Table 3',
+ 'ln-all-text' : 'All',
+ 'beers' : 'Beers',
+ 'spirits' : 'Spritis',
+ 'wines' : 'Wines',
+ 'ln-checkout-text' : 'Checkout',
+ 'ln-undo-text' : 'Undo',
+ 'ln-redo-text' : 'Redo' 
+  },
+  historyPage: {
+   'ln-home-menu' : 'Home',
+ 'ln-history-menu' : 'History',
+ 'ln-inventory-menu' : 'Inventory',
+ 'ln-logout-menu' : 'Logout',
+ 'ln-transaction-header-text':'Transaction Id',
+  'ln-userid-header-text' :'User Id',
+  'ln-beverageid-header-text':'Beverage Id',
+  'ln-date-header-text':'Date',
+  'ln-transaction-footer-text':'Transaction Id',
+  'ln-userid-footer-text':'User Id',
+  'ln-beverageid-footer-text':'Beverage Id',
+   'ln-date-footer-text':'Date'
+  }
+  },
+  de: {
+    title: "den flygande holländaren",
+    welcomeMessage: "Välkommen till den flygande holländaren!",
+    "login-h1": "logga in",
+    undoButton: "Ångra",
+    redoButton: "Göra om",
+    resetButton: "Återställ databasen",
+    HistoryBtn: "Historia",
+    InventoryBtn: "Lager",
+    username: "Användarnamn",
+    password: "Lösenord",
+    signin: "Logga in",
+    "signup-h1": "Bli Medlem",
+    allTab: "Allt",
+    beerTab: "Öl",
+    spiritTab: "Sprit",
+    wineTab: "Vin",
+    customerButton : "Kund",
+    barButton : "Barman",
+    checkout: "kolla upp",
+    orderHeading: "Mina beställningar",
+    credit: "Kreditera: ",
+    yes: "Ja",
+    no: "Nej",
+    recharge: "Belopp",
+    rechargeButton: "Ladda om",
+    signout: "Logga ut",
+    vip: "VIP-inloggning",
+    availability: "I lager : ",
+    menuTabs: {
+      allDrink: "Alla drycker",
+      beer: "Öl",
+      wine: "vin",
+      spirit: "Sprit",
+      myFavourite: "Mina favoriter"
+    },
+    cashier: {
+      cashierText: "Kolla upp",
+      payButton: "Betala",
+      receiptButton: "Mottagande",
+      checkoutText: "Släpp beställningen här för att betala!"
+    },
+    table: {
+      t1Name: "bord 1",
+      t2Name: "Tabell 2",
+      t3Name: "Tabell 3"
+    },
+	Inventory: {
+		OrderID: "Beställnings-ID",
+		BeverageName: "Dryckens namn",
+		Amount:"Belopp",
+		Alcohol:"Alkohol",
+		Category:"Kategori",
+		Quantity:"Kvantitet"
+	},
+	OrderHistory: {
+		OrderID: "Beställnings-ID",
+		BeverageName: "Dryckens namn",
+		OrderBy : "Sortera efter",
+		Amount:"Belopp",
+		Alcohol:"Alkohol",
+		OrderTime: "Beställ tid",
+		Category:"Kategori"
+	},
+    order: {
+      orderText: "Totala summan:"
+    },
+	Report:{
+		ReportIssue: "Rapportera problem"
+	},
+  bartenderHome: {
+  'ln-home-menu' : 'Hem',
+ 'ln-history-menu' : 'Historia',
+ 'ln-inventory-menu' : 'Lager',
+ 'ln-logout-menu' : 'Logga ut',
+ 'ln-table1-text': 'Tablell 1',
+ 'ln-total-amount-text1': 'Totala summan',
+ 'ln-total-amount-text2': 'Totala summan',
+ 'ln-total-amount-text3': 'Totala summan',
+ 'ln-table2-text' : 'Tabell 2',
+ 'ln-table3-text' : 'Tabell 3',
+ 'all' : 'Allt',
+ 'beers' : 'Öl',
+ 'spirits' : 'Sprit',
+ 'wines' : 'Vin',
+ 'ln-checkout-text' : 'Kolla upp',
+ 'ln-undo-text' : 'ångra',
+ 'ln-redo-text' : 'göra om'
+  },
+historyPage: {
+  'ln-home-menu' : 'Hem',
+ 'ln-history-menu' : 'Historia',
+ 'ln-inventory-menu' : 'Lager',
+ 'ln-logout-menu' : 'Logga ut',
+ 'ln-transaction-header-text':'Transaktions ID',
+  'ln-userid-header-text' :'Användar ID',
+  'ln-beverageid-header-text':'Dryck Id',
+  'ln-date-header-text':'Datum',
+  'ln-transaction-footer-text':'Transaktions ID',
+  'ln-userid-footer-text':'Användar ID',
+  'ln-beverageid-footer-text':'Dryck Id',
+   'ln-date-footer-text':'Datum'
+  }
+  }
+};
